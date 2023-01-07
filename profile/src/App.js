2296575be28a84e003f8f5331c3413984211dcc4
Maira Pajaro <maira_pajaro@hotmail.com>
2023-01-07 23:25:04 +0100

    profilecrisupdated

--- profile/src/App.js
+++ profile/src/App.js
@@ -1,11 +1,25 @@
-import Landing from './components/Landing';
-import './App.css';
+import { useContext } from "react";
+import Landing from "./components/Landing";
+import Toggle from "./components/Toggle";
+import { ThemeContext } from "../src/context";
+import About from "./components/About";
+import Entreviews from "./components/Entreviews";
 
 function App() {
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
   return (
-    <>
-    <Landing/>
-    </>
+    <div
+      style={{
+        backgroundColor: darkMode ? "#121212" : "#ededed",
+        color: darkMode && "#ededed",
+      }}
+    >
+      <Toggle />
+      <Landing />
+      <About />
+      <Entreviews/>
+    </div>
   );
 }
 
---
+++ profile/src/data.js
@@ -0,0 +1,36 @@
+const data = [
+  {
+    id: 1,
+    img: "./images/elfutbolverdadero.png",
+    link: "https://www.elfutbolverdadero.com/futbolverdadero-entrevista-a-cristian-micale-el-futbol-es-un-juego-y-el-elemento-fundamental-es-la-pelota/",
+  },
+
+  {
+    id: 2,
+    img: "./images/pilaradiario.png",
+    link: "https://www.pilaradiario.com/laliga/2021/5/26/un-pilarense-suena-con-dirigir-en-espana-111166.html",
+  },
+
+  {
+    id: 3,
+    img: "./images/futbolbcnnetEntrevista.png",
+    link: "https://www.futbolbcn.net/entrevista-cristian-y-maira-queremos-estar-donde-los-mejores/",
+  },
+
+  // {
+  //   id: 4,
+  //   img: './images/ivanChavezConversatorio.png',
+  //   link: "https://www.youtube.com/watch?v=NmG5E47jhvY",
+  // },
+  // {
+  //   id: 5,
+  //   img: "./images/pilartelered.png",
+  //   link: "https://www.youtube.com/watch?v=Otf0abABjEc&t=12s",
+  // },
+  // {
+  //   id: 6,
+  //   img: './images/pinasportConversatorio.png',
+  //   link: "https://www.facebook.com/watch/live/?ref=watch_permalink&v=3217499938336496",
+  // },
+];
+export default data;
